25986ba0b5edc86e79d2e831b63b3fe7e37d5f76
Natalya Moiseeva <natalyamoiseeva@MacBook-Air-Natalya.local>
2025-05-12 16:04:43 +0300

    ver 3.1.3

--- bot_logic/export.py
+++ bot_logic/export.py
@@ -15,7 +15,6 @@ from telegram.ext import (
 
 from .links import responses
 
-# /export (только для ADMIN_IDS)
 async def export(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.effective_user.id
     if user_id not in ADMIN_IDS:
@@ -23,20 +22,25 @@ async def export(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
 
     if not responses:
-        await update.message.reply_text("📭 Пока нет данных для экспорта.")
+        await update.message.reply_text("📭 Пока нет новых данных для экспорта.")
         return
 
     file_path = "export.xlsx"
+    new_df = pd.DataFrame(responses)
 
-    # Проверка: если файл уже существует – загружаем, иначе создаем
     if os.path.exists(file_path):
         existing_df = pd.read_excel(file_path)
-        new_df = pd.DataFrame(responses)
         combined_df = pd.concat([existing_df, new_df], ignore_index=True)
     else:
-        combined_df = pd.DataFrame(responses)
+        combined_df = new_df
 
+    # Удаляем дубликаты
+    combined_df.drop_duplicates(
+        subset=["username", "category", "age", "time", "date"],
+        inplace=True
+    )
 
+    # Переименование столбцов
     combined_df = combined_df.rename(columns={
         "username": "Никнейм",
         "category": "Психолог",
@@ -45,26 +49,15 @@ async def export(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "date": "Дата заполнения"
     })
 
+    # Сохраняем в файл с автошириной
     with pd.ExcelWriter(file_path, engine="openpyxl") as writer:
         combined_df.to_excel(writer, index=False, sheet_name="Ответы")
         worksheet = writer.sheets["Ответы"]
 
-    # # Автоматическая ширина столбцов — ВНУТРИ блока writer
-    # for col_num, column in enumerate(combined_df.columns, 1):
-    #     if combined_df[column].empty:
-    #         max_length = len(column) + 2
-    #     else:
-    #         max_length = max(
-    #             combined_df[column].astype(str).map(len).max(),
-    #             len(column)
-    #         ) + 2
-    #     col_letter = get_column_letter(col_num)
-    #     worksheet.column_dimensions[col_letter].width = max_length
-
+        from openpyxl.utils import get_column_letter
+        for col_num, column in enumerate(combined_df.columns, 1):
+            max_length = max(combined_df[column].astype(str).map(len).max(), len(column)) + 2
+            col_letter = get_column_letter(col_num)
+            worksheet.column_dimensions[col_letter].width = max_length
 
     await update.message.reply_document(document=open(file_path, "rb"), filename="results.xlsx")
-
-# /cancel
-async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Опрос отменён.", reply_markup=ReplyKeyboardRemove())
-    return ConversationHandler.END
